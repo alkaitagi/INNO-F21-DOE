8d636775776d757776a88bef17cf3e9fcdfe6e6c
alkaitagi <alkaitagi@outlook.com>
2021-08-16 16:06:59 +0300

    Refactor

--- Lab 1/app_python/main.py
+++ Lab 1/app_python/main.py
@@ -2,13 +2,40 @@ from flask import Flask
 import requests
 from datetime import datetime
 
-API_URL = "http://worldtimeapi.org/api/timezone/Europe/Moscow"
 app = Flask(__name__)
+
+
+def get_time_string(place: str = 'Europe/Moscow'):
+    '''
+    Get time data from worldtimeapi.org and return simple string
+
+    Parameters
+    ----------
+    place : str
+        Location, i.e. 'Europe/Moscow'.
+
+    Returns
+    -------
+    string
+        Time in format '%Y-%m-%d %H:%M:%S'
+
+    Examples
+    --------
+    >>> get_time_string()
+    2021-08-16 16:03:34
+    '''
+
+    url = "http://worldtimeapi.org/api/timezone/"+place
+    data = requests.get(url).json()
+    date = datetime.fromisoformat(data["datetime"])
+    string = date.strftime('%Y-%m-%d %H:%M:%S')
+    return string
 
 
 @app.route("/")
 def show_time():
-    data = requests.get(API_URL).json()
-    date = datetime.fromisoformat(data["datetime"])
-    text = date.strftime('%Y-%m-%d %H:%M:%S')
-    return f"<p>{text}</p>"
+    '''
+    Fetch Moscow time string and display it in a paragraph
+    '''
+    time = get_time_string('Europe/Moscow')
+    return f"<p>{time}</p>"
